dfa63e2b56c316ead0c72db5b997b69e59fcd93f
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-02-11 23:21:45 -0300

    Inicia Req01

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,46 @@
-function index() {
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { createUser } from '../../services/userAPI';
+
+function Login() {
+  const navigate = useNavigate();
+  const [button, setButton] = useState(true);
+  const [name, setName] = useState('');
+
+  function onChange({ target }: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = target;
+
+    if (value.length >= 3) {
+      setButton(false);
+      setName(value);
+    } else { setButton(true); }
+  }
+
+  function onClick() {
+    const user = { name };
+    createUser(user);
+    navigate('/search');
+  }
+
   return (
-    <div>index</div>
+    <main>
+      <input
+        type="text"
+        placeholder="Digite seu nome"
+        name="name"
+        data-testid="login-name-input"
+        onChange={ onChange }
+      />
+
+      <button
+        data-testid="login-submit-button"
+        disabled={ button }
+        onClick={ onClick }
+      >
+        Entrar
+      </button>
+    </main>
   );
 }
 
-export default index;
+export default Login;
