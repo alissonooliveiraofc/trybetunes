2988eea32353e081a950a0969aef6c719a00862c
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-02-20 20:47:30 -0300

    REQ 3 feito

--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -1,24 +1,47 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getMusics from '../../services/musicsAPI';
+import Loading from '../../components/Loading';
+import MusicCard from '../../components/MusicCard';
 
 function Album() {
   const { id } = useParams();
-  const [allAlbuns, setAllAlbuns] = useState<any[]>([]);
+  const [allAlbuns, setAllAlbuns] = useState<any[]>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMusics = async () => {
       if (id) {
         const albums = await getMusics(id);
         setAllAlbuns(albums);
-        // console.log(allAlbuns);
+        setLoading(false);
       }
     };
     fetchMusics();
-  }, [id, allAlbuns]);
+  }, [id]);
+
+  // console.log(allAlbuns);
 
   return (
-    <h1>Album</h1>
+    <main>
+      { allAlbuns && (
+        <div>
+          <img src={ allAlbuns[0].artworkUrl60 } alt="" />
+          <h1 data-testid="album-name">{allAlbuns[0].collectionName}</h1>
+          <h2 data-testid="artist-name">{allAlbuns[0].artistName}</h2>
+        </div>
+      )}
+      { allAlbuns && allAlbuns.slice(1).map((album) => (
+        <div key={ album.trackName }>
+          <MusicCard
+            trackName={ album.trackName }
+            trackId={ album.trackId }
+            previewUrl={ album.previewUrl }
+          />
+        </div>
+      ))}
+      {loading && <Loading />}
+    </main>
 
   );
 }
