0e20bf96bbab5101492d08e7f80c655ed9933079
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-02-20 21:59:29 -0300

    req 4 - cria componente header e layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ import { getUser } from '../services/userAPI';
 function Header() {
   return (
     <header data-testid="header-component">
-      <NavLink data-testid="link-to-search" to="/search" />
-      <NavLink data-testid="link-to-favorites" to="/favorites" />
-      <NavLink data-testid="link-to-profile" to="/profile" />
+      <NavLink data-testid="link-to-search" to="/search">Pesquisa</NavLink>
+      <NavLink data-testid="link-to-favorites" to="/favorites">Favoritas</NavLink>
+      <NavLink data-testid="link-to-profile" to="/profile">Perfil</NavLink>
 
-      <p>
+      <p data-testid="header-user-name">
         Bem vindo(a)
         {' '}
       </p>
@@ -17,3 +17,4 @@ function Header() {
 }
 
 export default Header;
+// pesquisa, favoritos e perfil
