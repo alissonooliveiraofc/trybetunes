59f413df1f5fdbefd5dfa847b7ac727bc4f396ad
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-02-23 14:37:27 -0300

    req 4 - concluido

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 function Header() {
+  const [userName, setUserName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUser();
+      setUserName(user.name);
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, [userName]);
+
+  if (isLoading) return <Loading />;
   return (
+
     <header data-testid="header-component">
       <NavLink data-testid="link-to-search" to="/search">Pesquisa</NavLink>
       <NavLink data-testid="link-to-favorites" to="/favorites">Favoritas</NavLink>
@@ -11,6 +27,7 @@ function Header() {
       <p data-testid="header-user-name">
         Bem vindo(a)
         {' '}
+        {userName}
       </p>
     </header>
   );
