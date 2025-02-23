0dc3a3d833ae5341899a9e777bb3ea8989cc4681
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-02-23 15:05:40 -0300

    req 5 - completo

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import { SongType } from '../types';
+import checkedHeart from '../images/checked_heart.png';
+import emptyHeart from '../images/empty_heart.png';
 
 function MusicCard({ trackName, previewUrl, trackId }: SongType) {
+  const [favorited, setFavorited] = useState<boolean>(false);
+
+  const handleFavoriteToggle = () => {
+    setFavorited(!favorited);
+  };
+
   return (
     <div>
       <p>{trackName}</p>
@@ -8,12 +17,23 @@ function MusicCard({ trackName, previewUrl, trackId }: SongType) {
         <track kind="captions" />
         O seu navegador não suporta o elemento
         {' '}
-        {' '}
         <code>audio</code>
-
       </audio>
-      <label htmlFor="" data-testid={ `checkbox-music-${trackId}` }>
-        <input type="checkbox" name="" id="" />
+      <label
+        htmlFor={ `checkbox-music-${trackId}` }
+        data-testid={ `checkbox-music-${trackId}` }
+      >
+        <input
+          type="checkbox"
+          name={ `checkbox-music-${trackId}` }
+          id={ `checkbox-music-${trackId}` }
+          checked={ favorited }
+          onChange={ handleFavoriteToggle }
+        />
+        <img
+          src={ favorited ? checkedHeart : emptyHeart }
+          alt="favorite"
+        />
       </label>
     </div>
   );
