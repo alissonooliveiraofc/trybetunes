05f05076012b2e38fd7f4bc9d1eeda45a2e492b8
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-03-03 16:32:24 -0300

    algumas mudanças css

--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import getMusics from '../../services/musicsAPI';
 import Loading from '../../components/Loading';
 import MusicCard from '../../components/MusicCard';
+import './styles.css';
 
 function Album() {
   const { id } = useParams();
@@ -21,25 +22,38 @@ function Album() {
   }, [id]);
 
   return (
-    <main>
-      { allAlbuns && (
-        <div>
-          <img src={ allAlbuns[0].artworkUrl60 } alt="" />
-          <h1 data-testid="album-name">{allAlbuns[0].collectionName}</h1>
-          <h2 data-testid="artist-name">{allAlbuns[0].artistName}</h2>
-        </div>
-      )}
-      { allAlbuns && allAlbuns.slice(1).map((album) => (
-        <div key={ album.trackName }>
-          <MusicCard
-            trackName={ album.trackName }
-            trackId={ album.trackId }
-            previewUrl={ album.previewUrl }
-          />
-        </div>
-      ))}
-      {loading && <Loading />}
-    </main>
+    <div className="search-container">
+      <div className="input-container">
+        { allAlbuns && (
+          <div className="album-info">
+            <img
+              src={ allAlbuns[0].artworkUrl100 }
+              alt=""
+              style={ {
+                width: '170px',
+                height: '170px',
+              } }
+            />
+            <h1 data-testid="album-name">{allAlbuns[0].collectionName}</h1>
+            <h2 data-testid="artist-name">{allAlbuns[0].artistName}</h2>
+          </div>
+        )}
+
+      </div>
+
+      <div className="result-container">
+        { allAlbuns && allAlbuns.slice(1).map((album) => (
+          <div key={ album.trackName }>
+            <MusicCard
+              trackName={ album.trackName }
+              trackId={ album.trackId }
+              previewUrl={ album.previewUrl }
+            />
+          </div>
+        ))}
+        {loading && <Loading />}
+      </div>
+    </div>
 
   );
 }
